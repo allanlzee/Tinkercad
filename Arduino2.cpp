6e9ace7ecdd653a811e4be3004568e6f4ad7d56e
Allan Z <74578068+allanlzee@users.noreply.github.com>
2021-06-11 16:29:20 -0400

    Update Arduino2.cpp

--- Arduino2.cpp
+++ Arduino2.cpp
@@ -4,6 +4,7 @@
 #define NOTE_G_SHARP 6
 #define NOTE_A_SHARP 7
 #define PIEZO 2
+#define POT 0
 
 // frequencies of notes 
 double C_SHARP = 0.0;
@@ -13,6 +14,8 @@ double G_SHARP = 0.0;
 double A_SHARP = 0.0;
 double C_HIGH = 0.0;
 
+int state;
+double octaveMultiplier;
 
 void setup()
 {
@@ -23,29 +26,49 @@ void setup()
   pinMode(NOTE_A_SHARP, INPUT); 
   Serial.begin(9600); 
   
-  C_SHARP = 277.18;
-  D_SHARP = 311.13;
-  F_SHARP = 369.99;
-  G_SHARP = 415.30;
-  A_SHARP = 466.16;
+  C_SHARP = 69.30;
+  D_SHARP = 73.42;
+  F_SHARP = 92.50;
+  G_SHARP = 103.83;
+  A_SHARP = 116.54;
+  
+  state = 0;
+  octaveMultiplier = 0.0;
 }
 
 void loop()
 {
+  
+  if (analogRead(POT) >= 0 && analogRead(POT) < 164){
+    state = 0;
+  } else if (analogRead(POT) >= 164 && analogRead(POT) < 348){
+    state = 1;  
+  } else if (analogRead(POT) >= 348 && analogRead(POT) < 511){
+    state = 2;  
+  } else if (analogRead(POT) >= 511 && analogRead(POT) < 675){
+    state = 3;  
+  } else if (analogRead(POT) >= 675 && analogRead(POT) < 859){
+	state = 4;  
+  } else if (analogRead(POT) >= 859 && analogRead(POT) < 1023){
+	state = 5;
+  }
+  
+  octaveMultiplier = pow(2, state);
+  
   if (digitalRead(NOTE_C_SHARP) == 1) {
-    tone(PIEZO, C_SHARP, 250);	// plays note c#
+    tone(PIEZO, C_SHARP * octaveMultiplier, 250);	// plays note c#
     
   } else if (digitalRead(NOTE_D_SHARP) == 1) {
-    tone(PIEZO, D_SHARP, 250);	// plays note d#
+    tone(PIEZO, D_SHARP * octaveMultiplier, 250);	// plays note d#
     
   } else if (digitalRead(NOTE_F_SHARP) == 1) {
-    tone(PIEZO, F_SHARP, 250);	// plays note f#
+    tone(PIEZO, F_SHARP * octaveMultiplier, 250);	// plays note f#
     
   } else if (digitalRead(NOTE_G_SHARP) == 1) {
-    tone(PIEZO, G_SHARP, 250);	// plays note g#
+    tone(PIEZO, G_SHARP * octaveMultiplier, 250);	// plays note g#
     
   } else if (digitalRead(NOTE_A_SHARP) == 1) {
-    tone(PIEZO, A_SHARP, 250);	// plays note a#
+    tone(PIEZO, A_SHARP * octaveMultiplier, 250);	// plays note a#
     
   } else {
     noTone(PIEZO);
