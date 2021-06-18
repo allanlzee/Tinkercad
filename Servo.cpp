8e75dba415e14d5f82c8140ed5f4386811934c83
Allan Z <74578068+allanlzee@users.noreply.github.com>
2021-06-18 15:22:37 -0400

    Update Servo.cpp

--- Servo.cpp
+++ Servo.cpp
@@ -1,21 +1,29 @@
 #include <Servo.h>
 #define SERVO1 13
 #define SERVO2 12
+#define SERVO3 11
+#define SERVO4 10
 
 Servo servo1; 
 Servo servo2; 
+Servo servo3; 
+Servo servo4; 
 
 int position = 0; 
 
 void setup() {
   servo1.attach(SERVO1); 
   servo2.attach(SERVO2); 
+  servo3.attach(SERVO3); 
+  servo4.attach(SERVO4); 
 }
 
 void loop() {
   for (position = 0; position <= 180; position++) {
     servo1.write(position); 
     servo2.write(position); 
+    servo3.write(position); 
+    servo4.write(position); 
     delay(15);
   }
   
@@ -24,6 +32,8 @@ void loop() {
   for (position = 180; position >= 0; position--) {
     servo1.write(position);
     servo2.write(position); 
+    servo3.write(position); 
+    servo4.write(position); 
     delay(15); 
   }
 }
