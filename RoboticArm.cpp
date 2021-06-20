89a3be2f29524be2d5794cdb839cb6b855f3fc8f
Allan Z <74578068+allanlzee@users.noreply.github.com>
2021-06-20 13:03:32 -0400

    Update RoboticArm.cpp

--- RoboticArm.cpp
+++ RoboticArm.cpp
@@ -5,11 +5,13 @@
 #define SERVO3 11
 #define SERVO4 10 
 
+// Initialize Servos
 Servo servo1; 
 Servo servo2; 
 Servo servo3; 
 Servo servo4; 
 
+// Initialize Potentiometer Values and Angles
 int servo_value_1; 
 int servo_value_2; 
 int servo_value_3; 
@@ -22,6 +24,7 @@ double angle4;
 
 void setup()
 {
+  // Attach Servos to Digital Pins
   Serial.begin(9600); 
   servo1.attach(SERVO1); 
   servo2.attach(SERVO2); 
@@ -31,20 +34,23 @@ void setup()
 
 void loop()
 {
+  // Read in Potentiometer
   servo_value_1 = analogRead(A4); 
   servo_value_2 = analogRead(A3); 
   servo_value_3 = analogRead(A2); 
   servo_value_4 = analogRead(A1); 
   
-  angle1 = map(servo_value_1, 0, 1023, 0, 273); 
-  angle2 = map(servo_value_2, 0, 1023, 0, 273); 
-  angle3 = map(servo_value_3, 0, 1023, 0, 273); 
-  angle4 = map(servo_value_4, 0, 1023, 0, 273); 
+  // Convert the range of 0 -> 1023 to 0 -> 546, which allows for angles 0 -> 360
+  angle1 = map(servo_value_1, 0, 1023, 0, 546); 
+  angle2 = map(servo_value_2, 0, 1023, 0, 546); 
+  angle3 = map(servo_value_3, 0, 1023, 0, 546); 
+  angle4 = map(servo_value_4, 0, 1023, 0, 546); 
   
+  // Give the servo its respective angle
   servo1.write(angle1); 
   servo2.write(angle2); 
   servo3.write(angle3); 
   servo4.write(angle4);
   
-  delay(15); 
+  delay(20); 
 }
